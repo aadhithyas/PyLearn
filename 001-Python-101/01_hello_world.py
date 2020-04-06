ad7a8882485581251b21fe4e6b1809a775a5e439
Rajmohan <ram.rajmohanr@gmail.com>
2020-04-07 08:37:48 +1000

    code updated

---
+++ 001-Python-101/01_hello_world.py
@@ -0,0 +1,11 @@
+
+print( "Hello Ajanthan, Welcome to the world of Python Programming")
+
+name = 'Ajanthan'
+language = 'Python'
+
+print("Hello "+ name + ", Welcome to the world of " + language + " Programming")
+
+print("Hello {}, Welcome to the world of {} Programming".format(name, language))
+
+print("Hello {username}, Welcome to the world of {lang} Programming".format(lang=language, username=name))
