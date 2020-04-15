d43a635302428ed06da8f0b08871d59d738a788e
Rajmohan <ram.rajmohanr@gmail.com>
2020-04-15 19:14:26 +1000

    code updated

---
+++ 002_Data_Types/08_challenge_01.py
@@ -0,0 +1,12 @@
+"""
+Challenge - 01                      
+
+Given input:
+    ip_address = "192.168.0.1"
+    subnet_mask = "255.255.255.0"
+
+Required output:                    
+    ip_address_cidr = "192.168.0.1/24"
+    ip_network = "192.168.0.0/24"
+"""
+# write down your code here and try to get the output required !!!
