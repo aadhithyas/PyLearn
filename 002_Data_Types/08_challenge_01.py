5bb4e5888c311d475bd06e29f74c971f88846ee1
Rajmohan R <ram.rajmohanr@gmail.com>
2020-04-20 10:24:16 +1000

    code updated

--- 002_Data_Types/08_challenge_01.py
+++ 002_Data_Types/08_challenge_01.py
@@ -10,6 +10,7 @@ Required output:
 
 """
 # write down your code here and try to get the output required !!!
+
 # Code Logic:
 # convert Subnet mask to binary
 # count the number of '1' from left - This is the prefix length
@@ -17,24 +18,29 @@ Required output:
 # ip address and mask variables
 ip_address = "192.168.0.1"
 subnet_mask = "255.255.255.0"
+print("Subnet Mask:", subnet_mask)
 
 # create the octet list - split the string 'subnet_mask' into 4 octets : delimiter '.'
 octets_list = subnet_mask.split('.')
+print("List of items (string):", octets_list)
 
-# convert all the items in the list to integer - using built-in function map()
+# All 4 items in the list is of type 'string'
+# Only integers can be converted into binary using bin() function
+# convert all the 4 items in the list to integer - using built-in function map()
 octets_list = list(map(int, octets_list))
+print("List of items (integer):", octets_list)
 
 # convert each item in the list to binary - Using list comprehension
 binary_list = [bin(i)[2:].zfill(8) for i in octets_list]
-print(binary_list)
+print("List of items (binary):", binary_list)
 
 # Join all the items in the list to form a string
 binary_string = "".join(binary_list)
-print(binary_string)
+print("Binary string:", binary_string)
 
 # prefix length - count the number of '1' in binary_string
 prefix_length = binary_string.count('1')
-print(prefix_length)
+print("Prefix Length:", prefix_length)
 
 # IP address in CIDR format
 print("IP address in CIDR format is {}/{}".format(ip_address, prefix_length))
--- 002_Data_Types/09_challenge_01_smart.py
+++ 002_Data_Types/09_challenge_01_smart.py
@@ -18,10 +18,12 @@ import ipaddress
 # ip address and mask variables
 ip_address = "192.168.0.1"
 subnet_mask = "255.255.255.0"
+print("Subnet Mask:", subnet_mask)
 
 # find prefix length using built-in 'ipaddress' module
 # https://docs.python.org/3/library/ipaddress.html
 prefix_length = ipaddress.IPv4Network("0.0.0.0/" + subnet_mask).prefixlen
+print("Prefix Length:", prefix_length)
 
 # IP address in CIDR format
 print("IP address in CIDR format is {}/{}".format(ip_address, prefix_length))
