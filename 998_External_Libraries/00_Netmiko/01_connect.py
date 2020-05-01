2ee27be1044e4c4aeae7bedffacfeb3fe41423eb
Rajmohan <ram.rajmohanr@gmail.com>
2020-05-01 17:20:08 +1000

    code updated

--- 998_External_Libraries/00_Netmiko/01_connect.py
+++ 998_External_Libraries/00_Netmiko/01_connect.py
@@ -1,5 +1,9 @@
 # import ConnectHandler from netmiko library
 from netmiko import ConnectHandler
+import time
+
+# start time of script
+start = time.perf_counter()
 
 # create device dictionary - details required to connect
 acc_sw_01 = {'device_type': 'cisco_ios', 'host':   '172.16.1.1', 'username': 'cisco', 'password': 'cisco',}
@@ -16,3 +20,7 @@ output = net_connect.send_command(cmd)
 # print the collected output
 print("\n---->  Output from {}:".format(acc_sw_01['host']))
 print(output)
+
+# finish time of script
+finish = time.perf_counter()
+print(f'\nTime take for completion of this script {round(finish - start)} second(s).')
